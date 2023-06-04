a15475c373c6b9e48763923267c423925ce3ff43
Thomas Bakken <tbakken14@gmail.com>
2023-06-04 14:36:53 -0700

    Fix default option in dropdown2 when switching dropdown1 selection

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,46 @@ let dropDown2;
 let section2;
 
 function selectDropDown1(event) {
+    //Reset drop down options
+    removeOptionsFromDropDown(dropDown2);
+    addOptionToDropDown(dropDown2, "Choose an option", true);
+    //Add new options
+    switch (dropDown1.value) {
+        case ("Software Engineering"):
+            addOptionToDropDown(dropDown2, "Frontend");
+            addOptionToDropDown(dropDown2, "Backend");
+            break;
+        case ("Data Science"):
+            addOptionToDropDown(dropDown2, "Database Management");
+            addOptionToDropDown(dropDown2, "Machine Learning");
+            addOptionToDropDown(dropDown2, "Statistics");
+            break;
+        default:
+            break;
+    }
     section2.removeAttribute("class");
-    newOption = document.createElement("option");
-    newValue = document.createTextNode("test");
-    newOption.appendChild(newValue);
-    dropDown2.appendChild(newOption);
 }
 
 function removeOptionsFromDropDown(dropDown) {
-    children = dropDown.children;
+    //Remove all options
     while (dropDown.children.length > 0) {
         child = dropDown.children[0];
         dropDown.remove(child);
     }
 }
 
-function addOptionToDropDown(dropDown, optionText) {
+function addOptionToDropDown(dropDown, optionText, isDefault = false) {
+    //Create option element
     optionElement = document.createElement("option");
     optionValue = document.createTextNode(optionText);
     optionElement.appendChild(optionValue);
+    //Set default properties
+    if (isDefault) {
+        optionElement.setAttribute("selected", "true");
+        optionElement.setAttribute("disabled", "disabled");
+        optionElement.setAttribute("hidden", "hidden");
+    }
+    //Add element to dropdown
     dropDown.appendChild(optionElement);
 }
 
