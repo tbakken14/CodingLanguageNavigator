01c8ac4390fde1c4b20cc0d7adb5a233f4248ae6
Thomas Bakken <tbakken14@gmail.com>
2023-06-04 14:14:50 -0700

    Add functions to add and remove selection options

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,42 @@
+//Global Variables
 let form;
 let dropDown1;
+let dropDown2;
 let section2;
 
 function selectDropDown1(event) {
-
     section2.removeAttribute("class");
+    newOption = document.createElement("option");
+    newValue = document.createTextNode("test");
+    newOption.appendChild(newValue);
+    dropDown2.appendChild(newOption);
+}
+
+function removeOptionsFromDropDown(dropDown) {
+    children = dropDown.children;
+    while (dropDown.children.length > 0) {
+        child = dropDown.children[0];
+        dropDown.remove(child);
+    }
+}
+
+function addOptionToDropDown(dropDown, optionText) {
+    optionElement = document.createElement("option");
+    optionValue = document.createTextNode(optionText);
+    optionElement.appendChild(optionValue);
+    dropDown.appendChild(optionElement);
 }
 
 //Initializations to execute on window load
 function InitializeVariables() {
     form = document.getElementsByTagName("form");
     dropDown1 = document.getElementById("input-1");
+    dropDown2 = document.getElementById("input-2");
     section2 = document.getElementById("section-2");
-    console.log(dropDown1);
 }
 
 function InitializeEventListeners() {
     dropDown1.addEventListener("change", selectDropDown1);
-    console.log(dropDown1);
 }
 
 window.onload = function () {
